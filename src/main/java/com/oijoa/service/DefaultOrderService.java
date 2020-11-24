ffe90601f3f487a2b1795b52e8202081b89bfd89
2seugnhyuck <rotid1818@gmail.com>
2020-11-24 20:35:56 +0900

    .

---
+++ src/main/java/com/oijoa/service/DefaultOrderService.java
@@ -0,0 +1,49 @@
+package com.oijoa.service;
+
+import java.util.List;
+import com.oijoa.dao.OrderDao;
+import com.oijoa.domain.Order;
+
+public class DefaultOrderService implements OrderService {
+
+  OrderDao orderDao;
+
+  public DefaultOrderService(OrderDao orderDao) {
+    this.orderDao = orderDao;
+  }
+
+  //  @Override
+  //  public int delete(int no) throws Exception {
+  //    return boardDao.delete(no);
+  //  }
+  //
+  //  @Override
+  //  public int add(Board board) throws Exception {
+  //    return boardDao.insert(board);
+  //  }
+  //
+  @Override
+  public List<Order> list() throws Exception {
+    return orderDao.findAll(null);
+  }
+
+  //  @Override
+  //  public List<Order> list(String keyword) throws Exception {
+  //    return orderDao.findAll(keyword);
+  //  }
+  //
+  //  @Override
+  //  public Board get(int no) throws Exception {
+  //    Board board = boardDao.findByNo(no);
+  //    if (board != null) {
+  //      boardDao.updateViewCount(no);
+  //    }
+  //    return board;
+  //  }
+  //
+  //  @Override
+  //  public int update(Board board) throws Exception {
+  //    return boardDao.update(board);
+  //  }
+
+}
