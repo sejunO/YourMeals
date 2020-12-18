5784323d917446e060783f2b0dfdd6604327ea87
sejunO <frame8916@gmail.com>
2020-12-18 21:33:50 +0900

    .

--- src/main/java/com/oijoa/web/NoticeAddServlet.java
+++ src/main/java/com/oijoa/web/NoticeAddServlet.java
@@ -1,57 +1,12 @@
 package com.oijoa.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import com.oijoa.domain.Notice;
-import com.oijoa.service.NoticeService;
 
 @WebServlet("/notice/add")
 public class NoticeAddServlet extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
 
-  @Override
-  protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
 
-
-    ServletContext ctx = request.getServletContext();
-    NoticeService noticeService = (NoticeService) ctx.getAttribute("noticeService");
-
-    request.setCharacterEncoding("UTF-8");
-    //    HttpSession session = request.getSession();
-
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<!DOCTYPE html>");
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<meta http-equiv='Refresh' content='1;list'>");
-    out.println("<title>공지사항 등록</title></head>");
-    out.println("<body>");
-    try {
-
-      Notice notice = new Notice();
-
-      notice.setNoticeTypeNo(Integer.parseInt(request.getParameter("type")));
-      notice.setTitle(request.getParameter("title"));
-      notice.setContent(request.getParameter("content"));
-      noticeService.add(notice);
-
-      out.println("게시글을 등록하였습니다.");
-
-    } catch (Exception e) {
-      request.setAttribute("exception", e);
-      request.getRequestDispatcher("/error.jsp").forward(request, response);
-    }
-    out.println("</body>");
-    out.println("</html>");
-  }
 }
---
+++ src/main/java/com/oijoa/web/QnaController.java
@@ -0,0 +1,69 @@
+package com.oijoa.web;
+
+import java.util.List;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.oijoa.domain.Qna;
+import com.oijoa.domain.User;
+import com.oijoa.service.QnaService;
+
+@Controller
+@RequestMapping("/mypage/qna")
+public class QnaController {
+
+  @Autowired
+  QnaService qnaService;
+
+  @RequestMapping("add")
+  public String add(HttpSession session, Qna qna) throws Exception {
+    User loginUser = (User) session.getAttribute("loginUser");
+    qna.setWriter(loginUser);
+
+    qnaService.add(qna);
+    return "redirect:list";
+  }
+
+  @RequestMapping("delete")
+  public String delete(int no) throws Exception {
+    Qna qna = qnaService.get(no);
+    if (qnaService.delete(no) == 0) {
+      throw new Exception("해당 번호의 게시글이 없습니다.");
+    }
+    return "redirect:list";
+  }
+
+  @RequestMapping("detail")
+  public ModelAndView detail(int no) throws Exception {
+    Qna qna = qnaService.get(no);
+    if (qna == null) {
+      throw new Exception("해당 번호의 게시글이 없습니다.");
+    }
+    ModelAndView mv = new ModelAndView();
+    mv.addObject("qna", qna);
+    mv.setViewName("/mypage/qna/detail.jsp");
+    return mv;
+  }
+
+  @RequestMapping("update")
+  public String update(Qna qna) throws Exception {
+    int count = qnaService.update(qna);
+    if (count == 0) {
+      throw new Exception("해당번호의 게시글이 없습니다.");
+    }
+    return "redirect:list";
+  }
+
+  @RequestMapping("list")
+  public ModelAndView list(HttpSession session) throws Exception {
+
+    List<Qna> list = qnaService.list();
+
+    ModelAndView mv = new ModelAndView();
+    mv.addObject("list", list);
+    mv.setViewName("/mypage/qna/list.jsp");
+    return mv;
+  }
+}
