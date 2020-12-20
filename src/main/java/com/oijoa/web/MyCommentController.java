0998c3491841b7e33beb29997a7686b7a16b5901
YuAram <sukgi5319@naver.com>
2020-12-21 00:02:14 +0900

    .

--- src/main/java/com/oijoa/web/MyCommentController.java
+++ src/main/java/com/oijoa/web/MyCommentController.java
@@ -12,20 +12,20 @@ import com.oijoa.service.CommentService;
 
 @Controller
 @RequestMapping("/mypage/comment")
- 
+
 public class MyCommentController {
   @Autowired
   CommentService commentService;
-  
+
   @RequestMapping("list")
   public ModelAndView list(HttpSession session) throws Exception {
     ModelAndView mv = new ModelAndView();
     User loginUser = (User) session.getAttribute("loginUser");
     List<Comment> commentList = commentService.userNoList(loginUser.getUserNo());
-    
+
     mv.addObject("commentList", commentList);
     mv.setViewName("/mypage/comment/list.jsp");
-    
+
     return mv;
   }
 }
