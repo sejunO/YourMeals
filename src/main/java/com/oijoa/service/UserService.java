8571a7e7d436ceef5e9fd99127fb3176a6241564
YuAram <sukgi5319@naver.com>
2020-12-21 14:14:58 +0900

    user join ing

--- src/main/java/com/oijoa/service/UserService.java
+++ src/main/java/com/oijoa/service/UserService.java
@@ -6,9 +6,14 @@ import com.oijoa.domain.User;
 public interface UserService {
 
   List<User> list() throws Exception;
+
   int update(User user) throws Exception;
+
   int delete(int no) throws Exception;
+
   User get(int no) throws Exception;
+
   User get(String email, String password) throws Exception;
 
+  int add(User user) throws Exception;
 }
