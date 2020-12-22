ebda5fdb5c6f03c507f4be08d287b415cab9dfec
YuAram <sukgi5319@naver.com>
2020-12-23 02:26:59 +0900

    .

--- src/main/java/com/oijoa/web/MyUserController.java
+++ src/main/java/com/oijoa/web/MyUserController.java
@@ -1,5 +1,6 @@
 package com.oijoa.web;
 
+import java.util.List;
 import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,14 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.oijoa.domain.Follow;
 import com.oijoa.domain.User;
+import com.oijoa.service.FollowService;
 import com.oijoa.service.UserService;
 
 @Controller
 @RequestMapping("/mypage/user")
 public class MyUserController {
-  @Autowired
-  UserService userService;
+  @Autowired UserService userService;
+  @Autowired FollowService followService;
 
   @GetMapping("detail")
   public void detail(HttpSession session, Model model) throws Exception {
@@ -50,5 +54,27 @@ public class MyUserController {
       throw new Exception("비밀번호 변경이 실패하였습니다.");
     }
     return "redirect:detail";
+  }
+
+  @RequestMapping("/followerList")
+  public ModelAndView followerList(HttpSession session) throws Exception {
+
+    User loginUser = (User) session.getAttribute("loginUser");
+    ModelAndView mv = new ModelAndView();
+    List<Follow> list = followService.FollowerList(loginUser.getUserNo());
+    mv.addObject("list", list);
+    mv.setViewName("/mypage/follow/follower/list.jsp");
+    return mv;
+  }
+
+  @RequestMapping("/followingList")
+  public ModelAndView followingList(HttpSession session) throws Exception {
+
+    User loginUser = (User) session.getAttribute("loginUser");
+    List<Follow> list = followService.FollowingList(loginUser.getUserNo());
+    ModelAndView mv = new ModelAndView();
+    mv.addObject("list", list);
+    mv.setViewName("/mypage/follow/following/list.jsp");
+    return mv;
   }
 }
