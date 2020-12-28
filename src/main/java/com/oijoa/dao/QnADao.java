18ed23926315b22e8310605a1a74d42c7fa2d7a1
YuAram <sukgi5319@naver.com>
2020-12-28 15:39:49 +0900

    mypage qna display btn Add

--- src/main/java/com/oijoa/dao/QnADao.java
+++ src/main/java/com/oijoa/dao/QnADao.java
@@ -9,4 +9,5 @@ public interface QnADao {
   List<Qna> findAll(String keyword) throws Exception;
   Qna findByNo(int no) throws Exception;
   int update(Qna qna) throws Exception;
+  List<Qna> findByUserNo(int no) throws Exception;
 }
